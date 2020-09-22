02ce9539e66b87a0df7ea25c3b5018e581e0bb67
Julia <juliacarnelli@gmail.com>
2020-09-21 22:36:26 -0300

    tela login

--- tela-login.js
+++ tela-login.js
@@ -1,3 +1,5 @@
+var token;
+
 var login = {
     "email": "lala@123.com",
     "password": "123456"
@@ -8,16 +10,48 @@ function validarLogin () {
     var email = document.querySelector(".email");
     var senha = document.querySelector(".senha");
 
-    console.log(email.value);
-    console.log(senha.value);
     if(email.value === "" || senha.value === ""){
         alert("Por favor preencha os campos corretamente!");
         return;
     }
-
+    login.email = email.value;
+    login.password = senha.value;
     enviarLoginServidor();
 }
 
 function enviarLoginServidor() {
-    var elemento
+    var elemento = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v1/buzzquizz/users", login);
+    elemento.then(loginSucesso).catch(loginIncorreto);
+    desabilitarBotao();
+        
+}
+
+function desabilitarBotao(){
+    var elemento = document.querySelector(".botao-login");
+    elemento.disabled = true;
+}
+
+function habilitarBotao(){
+    var elemento = document.querySelector(".botao-login");
+    elemento.disabled = false;
+}
+
+function irParaTelaAdmin() {
+    var elemento = document.querySelector(".login");
+    elemento.style.visibility = "hidden";
+    elemento = document.querySelector(".admin-quizzes");
+    elemento.style.visibility = "visible";
+}
+
+function loginSucesso (resposta){
+    token = resposta.data.token;
+    irParaTelaAdmin();
+    habilitarBotao();
+}
+
+function loginIncorreto (erro){
+    if(erro.response.status === 401){
+        alert("E-mail ou senha incorretos");
+    }
+    habilitarBotao();
 }
